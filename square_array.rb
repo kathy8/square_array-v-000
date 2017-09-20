a8c068d676b7ca6e5bc0387de5462cbff667e64f
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-20 17:43:51 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # yourdef square_array(array)
-  new_array = []
+  new_array = [1, 2, 3]
   array.each do |x|
-    new_array.push(a ** 2)
+    new_array.push(x ** 2)
   end
   new_array
 end
